03c6cc997182bac78099018b151c2e164b461990
otavio.ramos <otavio.ramos@tjsc.jus.br>
2022-09-20 16:41:42 -0300

    slider

--- script.js
+++ script.js
@@ -48,3 +48,44 @@ document.querySelector(".sub-menu").addEventListener('mouseover', ()=>{
     document.querySelector('.sub-menu').classList.remove('close'); 
     document.querySelector('#'+selected).classList.add('header-ul-li-hover');  
 });
+let index = 0;
+let dx =0;
+const left = document.querySelector(".icon-prev");
+left.addEventListener('click', () => {
+    if(index < 2 ){
+        let current = document.querySelector(".current-item");
+        let next = current.nextSibling;
+        console.log("vv " + next);
+        let width = current.getBoundingClientRect().width;
+        let ofertas_wrapper = document.querySelector('.ofertas-wrapper');
+        
+        current.classList.remove("current-item");
+        console.log("cc " + current.innerHTML);
+        
+        console.log("nc " + next.innerHTML);
+        next.classList.add("current-item");
+        index++;
+        dx-= width;
+        ofertas_wrapper.style.transform= "translateX("+(dx)+"px)";
+    }
+});
+
+const right = document.querySelector(".icon-next");
+right.addEventListener('click', () => {
+    if( index > 0 && index <=2 ){
+        let current = document.querySelector(".current-item");
+        let prev = current.previousSibling;
+        console.log("vv " + prev);
+        let width = current.getBoundingClientRect().width;
+        let ofertas_wrapper = document.querySelector('.ofertas-wrapper');
+        
+        current.classList.remove("current-item");
+        console.log("cc " + current.innerHTML);
+        
+        console.log("nc " + prev.innerHTML);
+        prev.classList.add("current-item");
+        index--;
+        dx+=width
+        ofertas_wrapper.style.transform= "translateX("+(dx)+"px)";
+    }
+});
